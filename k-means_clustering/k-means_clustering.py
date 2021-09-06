4d13a6e401775b97865dad3f2611c08630f336df
Shabab <ashababnoor@gmail.com>
2021-09-06 12:57:51 +0600

    made changes

--- k-means_clustering/k-means_clustering.py
+++ k-means_clustering/k-means_clustering.py
@@ -9,3 +9,37 @@ mycenters = genfromtxt((datapath+'centers.csv'), delimiter=',')
 # print(mydata.shape)
 # print(mycenters.shape)
 
+clusters = []
+temp_clusters = []
+
+for i in range(6):
+    clusters.append([])
+    temp_clusters.append([])
+
+# print(clusters)
+# print(temp_clusters)
+
+centers = mycenters.copy()
+
+iteration = 0
+
+while True:
+    for data in range(len(mydata)):
+        index = 0
+        min_dist = np.sum(np.square(mydata[data] - centers[0]))
+        for center in range(1, len(centers)):
+            distance = np.sum(np.square(mydata[data] - centers[center]))
+            if distance < min_dist:
+                min_dist = distance
+                index = center
+        
+        temp_clusters[index].append(data)
+
+    for cluster in temp_clusters:
+        array = np.array([mydata[ind] for ind in cluster])
+        centers = centers[1:]
+        
+
+
+
+
--- practice/testing.py
+++ practice/testing.py
@@ -1,9 +1,20 @@
 print('hello world')
 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+import numpy as np
 
 x = list(range(5))
 y = [3*i for i in range(5)]
 
-plt.plot(x, y)
-plt.show()
+# plt.plot(x, y)
+# plt.show()
+
+array = np.array([[2, 4, 1], [6, 13, 3], [7, 8, 3]])
+print(np.mean(array, axis=0))
+print(np.delete(array, 0))
+print(array[1:])
+
+print('=======')
+
+array = array[1:]
+print(array)
